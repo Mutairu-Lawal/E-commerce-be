ba42022e13d918c154ef4365fc10c93f0c9adc92
Mutairu <thebillionnairadream@gmail.com>
2026-01-31 16:44:28 +0100

    added new middlewares

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 // Sample route
 app.get('/', (req, res) => {
